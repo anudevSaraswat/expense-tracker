0c583f207346f9b330db4ceb3b2ea1a653a8d320
anudevSaraswat <mwanudev@gmail.com>
2024-06-16 14:41:52 +0530

    wip: button moved out of form

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,31 +1,44 @@
-export function AddExpenseForm() {
+import { useState } from "react";
+
+export function AddExpenseForm({ addExpense }) {
+
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
+
     return (
-        <form>
-            <div className="row">
-                <div className="col-sm">
-                    <label for="name">Name</label>
-                    <input required="required"
-                        type="text"
-                        className="form-control"
-                        id="name">
-                    </input>
+        <div>
+            <form>
+                <div className="row">
+                    <div className="col-sm">
+                        <label for="name">Name</label>
+                        <input required="required"
+                            type="text"
+                            className="form-control"
+                            id="name"
+                            onChange={(e) => { setName(e.target.value) }}>
+                        </input>
+                    </div>
+                    <div className="col-sm">
+                        <label for="cost">Cost</label>
+                        <input required="required"
+                            type="text"
+                            className="form-control"
+                            id="cost"
+                            onChange={(e) => { setCost(e.target.value) }}>
+                        </input>
+                    </div>
                 </div>
+                {/* <div className="row">
                 <div className="col-sm">
-                    <label for="cost">Cost</label>
-                    <input required="required"
-                        type="text"
-                        className="form-control"
-                        id="cost">
-                    </input>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-sm">
-                    <button type="submit" className="btn btn-primary mt-3">
+                    <button className="btn btn-primary mt-3" onClick={() => addExpense(name, cost)}>
                         Save
                     </button>
                 </div>
-            </div>
-        </form>
+            </div> */}
+            </form>
+            <button className="btn btn-primary mt-3" onClick={() => addExpense(name, cost)}>
+                Save
+            </button>
+        </div>
     );
 }
