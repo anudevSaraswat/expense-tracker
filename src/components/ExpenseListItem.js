57e26e53a36a19e2eb07e8200c8cbfcb4e377578
anudevSaraswat <mwanudev@gmail.com>
2024-06-16 16:27:55 +0530

    wip: cost is now visible in ExpenseListItem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,10 +4,10 @@ export function ExpenseListItem(props) {
     return (
         <li className="list-group-item d-flex justify-content-between align-items-center">
             {props.name}
+            <span>
+                {props.cost}INR
+            </span>
             <div>
-                <span className="badge badge-primary badge-pill mr-3">
-                    Rs{props.cost}
-                </span>
                 <TiDelete size="1.5em"></TiDelete>
             </div>
         </li>
